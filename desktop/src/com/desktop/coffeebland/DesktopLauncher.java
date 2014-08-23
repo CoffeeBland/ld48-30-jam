500ccdfe7ff437d476135df1840e0bb63d06dbfd
dagothig <guillaumenoelmartel@gmail.com>
2014-08-23 09:12:28 -0400

    Initializing repo

---
+++ desktop/src/com/desktop/coffeebland/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.desktop.coffeebland;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.coffeebland.HotSpot;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HotSpot(), config);
+	}
+}
