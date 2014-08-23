5d07e1000228639cc8ddca8a3e5f816dcafd6c96
dagothig <guillaumenoelmartel@gmail.com>
2014-08-23 10:19:59 -0400

    State manager

--- core/src/com/coffeebland/HotSpot.java
+++ core/src/com/coffeebland/HotSpot.java
@@ -2,26 +2,37 @@ package com.coffeebland;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.coffeebland.util.StateManager;
 
 public class HotSpot extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+    long time;
+    StateManager stateManager;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+        time = System.currentTimeMillis();
+        stateManager = new StateManager();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+        long current = System.currentTimeMillis();
+        stateManager.update((current - time));
+        time = current;
+
+        Color col = stateManager.getBackgroundColor();
+        Gdx.gl.glClearColor(col.r, col.g, col.b, col.a);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        stateManager.render();
+        batch.end();
+    }
 }
